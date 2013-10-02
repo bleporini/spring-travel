05d777494b7e9a9073fbbccc390217488316816b
bleporini <bleporini@gmail.com>
2013-10-02 07:44:32 +0200

    remove serial gc usage

---
+++ xebia-spring-travel-environment-installer/overrides/setenv.sh
@@ -0,0 +1,3 @@
+#export JAVA_OPTS="-client -XX:+PrintGCDetails -Xloggc:$CATALINA_HOME/logs/GC.log -XX:+UseSerialGC -Xmx512m -XX:MaxPermSize=128M"
+export JAVA_OPTS="-server -XX:+PrintGCDetails -Xloggc:$CATALINA_HOME/logs/GC2.log -Xmx512m -XX:MaxPermSize=128M"
+export CATALINA_OPTS=" -Dcom.sun.management.jmxremote -Dcom.sun.jmxremote.port=8084 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
