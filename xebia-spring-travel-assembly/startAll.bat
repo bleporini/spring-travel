99b2f170fc54c41acc95e32f03073b67a3022dff
Florent Ramière <florent@ramiere.com>
2014-01-15 22:37:07 +0100

    startall.bat

---
+++ xebia-spring-travel-assembly/startAll.bat
@@ -0,0 +1,17 @@
+echo Starting HSQLDB
+cd hsqldb/data/
+start java -Xmx124m -classpath ../hsqldb-${hsqldb.version}.jar org.hsqldb.server.Server --database.0 file:bookings --dbname.0 bookings --database.1 file:fraud --dbname.1 fraud
+
+cd ../..
+
+echo Starting Tomcat
+cd apache-tomcat/bin
+start catalina.bat run
+
+cd ../..
+
+echo Starting JMeter
+start apache-jmeter-${jmeter.version}/bin/jmeter -t apache-jmeter-${jmeter.version}/data/xebia-spring-travel.jmx
+
+echo Starting JConsole
+start jconsole
